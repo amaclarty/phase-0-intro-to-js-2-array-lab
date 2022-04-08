675fe907e0b15372f444f418aa14643c64d0c522
amaclarty <100883044+amaclarty@users.noreply.github.com>
2022-04-08 19:25:25 -0400

    commit tree eb990b77020a1cd55448386166e7e2a0ced30cdb

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(newcatpost)   {
+    cats.push(newcatpost);
+}
+
+let newcatpost = "Ralph"; 
+
+function destructivelyPrependCat(newcatpre) {
+    cats.unshift(newcatpre);
+}
+
+function destructivelyRemoveLastCat(lesscatpost) {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(lesscatpre) {
+    cats.shift();
+}
+let duplicats = [...cats];
+function appendCat(newcatcrew) {  
+    duplicats.push(newcatcrew);
+    return duplicats;
+}
+
+let newcatcrew = "Broom";
+
+let dupliduplicats = [...cats]
+function prependCat(frontguardcat) {
+    dupliduplicats.unshift(frontguardcat);
+    return dupliduplicats;
+};
+let frontguardcat = "Arnold";
+
+
+let backcatretirement = [... cats];
+function removeLastCat(retiredcat) {
+    backcatretirement.pop();
+    return backcatretirement;
+};
+
+let frontcatretirement = [...cats];
+function removeFirstCat(retiredcat) {
+    frontcatretirement.shift();
+    return frontcatretirement
+};
